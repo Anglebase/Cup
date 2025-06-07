226b48a8e5f7cc97633491a8725f750b97e6559d
Anglebase <1628818324@qq.com>
2025-06-07 13:58:42 +0800

    fix(config): 修正文件名的大小写错误

--- src/config.cpp
+++ src/config.cpp
@@ -3,7 +3,7 @@
 
 Config::Config(const fs::path &project)
 {
-    auto cup_toml = project / "Cup.toml";
+    auto cup_toml = project / "cup.toml";
     if (!fs::exists(cup_toml))
         throw std::runtime_error("'" + project.string() + "' is not a valid cup project.");
     std::ifstream ifs(cup_toml.string());
