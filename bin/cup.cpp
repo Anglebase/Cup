edadfc55f6bbf5aafcafa708de8b289b0620ea11
Anglebase <1628818324@qq.com>
2025-06-07 11:48:38 +0800

    feat(main): 添加程序执行主逻辑

---
+++ bin/cup.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include "cmd.h"
+#include "log.h"
+#include <unordered_map>
+#include <functional>
+#include "tools.h"
+
+int main(int argc, char **argv)
+{
+    const auto args = SysArgs(argc, argv);
+#ifdef _DEBUG
+    std::cout << "Arguments: ";
+    for (const auto &arg : args.getArguments())
+        std::cout << arg << " ";
+    std::cout << std::endl;
+    std::cout << "Flags: ";
+    for (const auto &flag : args.getFlags())
+        std::cout << flag << " ";
+    std::cout << std::endl;
+    std::cout << "Configs: " << std::endl;
+    for (const auto &[key, value] : args.getConfigs())
+    {
+        std::cout << key << " = ";
+        for (const auto &v : value)
+            std::cout << v << " ";
+        std::cout << std::endl;
+    }
+#endif
+    if (args.getArguments().empty())
+    {
+        LOG_DEBUG("Help| Error: {No Cmd}");
+        return 0;
+    }
+    auto cmd = args.getArguments()[0];
+    std::unordered_map<std::string, std::function<int(void)>> table = {
+        {"new", [&]()
+         {
+             NewCmd newCmd(args);
+             return newCmd.run();
+         }},
+        {"clean", [&]()
+         {
+             CleanCmd cleanCmd(args);
+             return cleanCmd.run();
+         }},
+        {"build", [&]()
+         {
+             BuildCmd buildCmd(args);
+             return buildCmd.run();
+         }},
+        {"run", [&]()
+         {
+             RunCmd runCmd(args);
+             return runCmd.exec();
+         }},
+    };
+    if (table.find(cmd) == table.end())
+    {
+        LOG_DEBUG("Help| Error: {No Cmd}");
+        return 0;
+    }
+    return table[cmd]();
+}
