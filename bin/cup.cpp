56eaddc61d1526da43bfa40069b4614769b9f775
Anglebase <1628818324@qq.com>
2025-06-10 19:42:01 +0800

    fix(main): 优化错误信息显示

--- bin/cup.cpp
+++ bin/cup.cpp
@@ -121,7 +121,7 @@ int main(int argc, char **argv)
     }
     catch (const std::exception &e)
     {
-        LOG_ERROR("Exception: ", e.what());
+        LOG_ERROR(e.what());
         return 1;
     }
 }
