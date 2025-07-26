4678938275064e187decde49add1d1c758d626b0
Anglebase <1628818324@qq.com>
2025-07-26 10:47:59 +0800

    Fix(build): Reduce the hierarchy of building directories.

--- src/res.cpp
+++ src/res.cpp
@@ -64,7 +64,7 @@ fs::path Resource::target(const fs::path &root)
 
 fs::path Resource::cmake(const fs::path &root)
 {
-    return build(root) / "cmake";
+    return Resource::build(root);
 }
 
 fs::path Resource::lib(const fs::path &root)
