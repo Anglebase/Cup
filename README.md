37065a9a3cfea05d65fa5a46f3199898ea19f7e0
Anglebase <1628818324@qq.com>
2025-09-12 17:25:25 +0800

    Cup-rs initiation

---
+++ README.md
@@ -0,0 +1,4 @@
+# Cup-rs
+
+
+**Note**: *`Cup-rs` is the implementation of the `Cup` by Rust. And in the future, the plan will completely shift towards Rust implementation.*
--- bin/log.cpp
+++
@@ -1,9 +0,0 @@
-#include "log.h"
-
-int main() {
-    LOG_DEBUG("Hello, world!", 123);
-    LOG_INFO("This is an info message.");
-    LOG_WARN("This is a warning message.");
-    LOG_ERROR("This is an error message.");
-    return 0;
-}
