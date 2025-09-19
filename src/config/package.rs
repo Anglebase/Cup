d7f31769b92761f321753e249b6f334397c46ab2
Anglebase <1628818324@qq.com>
2025-09-19 22:09:43 +0800

    添加package.workspace字段和package.publish字段

--- src/config/package.rs
+++ src/config/package.rs
@@ -1,4 +1,4 @@
-use std::path::Path;
+use std::path::{Path, PathBuf};
 
 use crate::{cli::TemplateType, config::Shelling};
 
@@ -8,14 +8,18 @@ pub struct PackageConfig {
     pub version: String,
     #[serde(rename = "type")]
     pub ty: TemplateType,
+    pub workspace: Option<PathBuf>,
+    pub publish: Option<bool>,
 }
 
 impl<P: AsRef<Path>> Shelling<Package, P> for PackageConfig {
-    fn shelling(self, _base: P) -> Package {
+    fn shelling(self, base: P) -> Package {
         Package {
             name: self.name,
             version: self.version,
             ty: self.ty,
+            workspace: self.workspace.unwrap_or(PathBuf::from(base.as_ref())),
+            publish: self.publish.unwrap_or(true),
         }
     }
 }
@@ -25,4 +29,6 @@ pub struct Package {
     pub name: String,
     pub version: String,
     pub ty: TemplateType,
+    pub workspace: PathBuf,
+    pub publish: bool,
 }
